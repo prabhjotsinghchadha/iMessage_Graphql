3aeb6b2ea05ad8b04b145d16f7c06017559ec6e8
Prabhjot <prabhjottechs@gmail.com>
2022-12-28 14:38:23 +0530

    incorrect syntax fixed

--- apps/backend/src/graphql/typeDefs/users.ts
+++ apps/backend/src/graphql/typeDefs/users.ts
@@ -1,22 +1,21 @@
-import { gql } from "graphql-tag";
+import gql from "graphql-tag";
 
 const typeDefs = gql`
-    type User {
-      id: String
-      username:String
-    }
+  scalar Date
+  type User {
+    id: String
+    username: String
+  }
+  type Query {
+    searchUsers(username: String!): [User]
+  }
+  type Mutation {
+    createUsername(username: String!): CreateUsernameResponse
+  }
+  type CreateUsernameResponse {
+    success: Boolean
+    error: String
+  }
+`;
 
-    type Query {
-      searchUser(username: String): [User]
-    }
-
-    type Mutation {
-      createUsername(username: String): CreateUsernameResponse
-    }
-
-    type CreateUsernameResponse {
-      sucess: Boolean
-      error: String
-    }
-`
 export default typeDefs;
