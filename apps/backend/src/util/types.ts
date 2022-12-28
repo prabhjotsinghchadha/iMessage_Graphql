b6dec8233c8289cc74a4030b281603f12a7b8450
Prabhjot <prabhjottechs@gmail.com>
2022-12-28 22:57:33 +0530

    removing unused components

--- apps/backend/src/util/types.ts
+++ apps/backend/src/util/types.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PubSub } from "graphql-subscriptions";
 import { Context } from "graphql-ws/lib/server";
 
