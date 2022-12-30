f0f78adac95b9413d37cbb386056a5504a83c8cc
Prabhjot <prabhjottechs@gmail.com>
2022-12-30 23:42:51 +0530

    adding validation for user

--- apps/backend/src/graphql/resolvers/user.ts
+++ apps/backend/src/graphql/resolvers/user.ts
@@ -1,11 +1,63 @@
+import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
+
 const resolvers = {
   Query: {
     searchUsers: () => { },
   },
   Mutation: {
-    createUsername: (_: any, args: { username: string }, context: any) => {
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<CreateUsernameResponse> => {
       const { username } = args;
-      console.log("Hey at the api", username)
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        return {
+          error: "Not authorized",
+        };
+      }
+
+      const { id: userId } = session.user;
+
+      try {
+        /**
+         * Check if username taken by another user
+         */
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            username
+          },
+        });
+
+        if (existingUser) {
+          return {
+            error: "Username already taken. Try another",
+          };
+        }
+
+        /**
+         * update username
+         */
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+
+        return { success: true };
+      } catch (error: any) {
+        console.log("createUsername error", error);
+        return {
+          error: error?.message as string,
+        };
+      }
+
+
 
     },
   },
