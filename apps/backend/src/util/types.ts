dcee5278ea7d2b0c4bfa637ef32b7ddc5ed2310a
Prabhjot <prabhjottechs@gmail.com>
2022-12-30 23:43:32 +0530

    updating types

--- apps/backend/src/util/types.ts
+++ apps/backend/src/util/types.ts
@@ -1,18 +1,18 @@
 import { PrismaClient } from "@prisma/client";
-import { PubSub } from "graphql-subscriptions";
 import { Context } from "graphql-ws/lib/server";
+import { ISODateString } from "next-auth";
 
 /**
  * Server Configuration
  */
 export interface Session {
   user?: User;
+  expires: ISODateString;
 }
 
 export interface GraphQLContext {
   session: Session | null;
   prisma: PrismaClient;
-  pubsub: PubSub;
 }
 
 export interface SubscriptionContext extends Context {
@@ -27,4 +27,13 @@ export interface SubscriptionContext extends Context {
 export interface User {
   id: string;
   username: string;
+  email: string;
+  emailVerified: boolean;
+  image: string;
+  name: string;
+}
+
+export interface CreateUsernameResponse {
+  success?: boolean;
+  error?: string;
 }
