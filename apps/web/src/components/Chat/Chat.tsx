e9b12115830de788f90c26bc2a3eef0496d94903
Prabhjot <prabhjottechs@gmail.com>
2023-01-01 17:43:27 +0530

    styling chat ui

--- apps/web/src/components/Chat/Chat.tsx
+++ apps/web/src/components/Chat/Chat.tsx
@@ -1,13 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import ConversationsWrapper from "./Conversations/ConversationsWrapper";
+import FeedWrapper from "./Feed/FeedWrapper";
 
-interface IChatProps { }
+interface ChatProps {
+  session: Session
+}
 
-const Chat: React.FC<IChatProps> = () => {
+const Chat: React.FC<ChatProps> = ({ session }) => {
   return (
-    <div>Chat
-      <Button onClick={() => signOut()}>Logout</Button>
-    </div>
+    <Flex height="100vh">
+      <ConversationsWrapper session={session} />
+      <FeedWrapper session={session} />
+    </Flex>
   );
 };
 
