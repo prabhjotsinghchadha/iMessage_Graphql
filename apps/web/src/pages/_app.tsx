2b27d664243d684a2a0a81b67fbdb22d7c98ea70
Prabhjot <prabhjottechs@gmail.com>
2023-01-01 16:50:22 +0530

    updating tost

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast';
 import { theme } from "../chakra/theme"
 import { client } from "../graphql/apollo-client";
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
       <SessionProvider session={session}>
         <ChakraProvider theme={theme}>
           <Component {...pageProps} />
+          <Toaster />
         </ChakraProvider>
       </SessionProvider>
     </ApolloProvider>
