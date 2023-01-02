100f8250d9e270e337a348633a443561e1473c3f
Prabhjot <prabhjottechs@gmail.com>
2023-01-02 21:50:24 +0530

    search user resolver data

--- apps/backend/src/graphql/resolvers/user.ts
+++ apps/backend/src/graphql/resolvers/user.ts
@@ -1,8 +1,41 @@
+import { User } from "@prisma/client";
+import { GraphQLError } from "graphql"
 import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
 
 const resolvers = {
   Query: {
-    searchUsers: () => { },
+    searchUsers: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<Array<User>> => {
+      const { username: searchedUsername } = args;
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        throw new GraphQLError("Not Authorized");
+      }
+
+      const {
+        user: { username: myUsername },
+      } = session;
+
+      try {
+        const users = await prisma.user.findMany({
+          where: {
+            username: {
+              contains: searchedUsername,
+              not: myUsername,
+              mode: 'insensitive'
+            },
+          },
+        });
+        return users;
+      } catch (error: any) {
+        console.log("searchUsers error", error);
+        throw new GraphQLError(error?.message);
+      }
+    },
   },
   Mutation: {
     createUsername: async (
