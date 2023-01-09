6fba8db47ef9976f677cf82f79d4caab115ee141
Prabhjot <prabhjottechs@gmail.com>
2023-01-10 00:08:26 +0530

    copied from demo

--- apps/web/src/components/Chat/Feed/FeedWrapper.tsx
+++ apps/web/src/components/Chat/Feed/FeedWrapper.tsx
@@ -1,6 +1,7 @@
 import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useRouter } from "next/router";
+import MessagesHeader from "./Messages/Header";
 
 interface FeedWrapper {
   session: Session;
@@ -8,11 +9,24 @@ interface FeedWrapper {
 
 const FeedWrapper: React.FC<FeedWrapper> = ({ session, }) => {
   const router = useRouter();
+  const { user: { id: userId } } = session;
 
   const { conversationId } = router.query;
   return (
     <Flex width="100%" direction='column' display={{ base: conversationId ? "flex" : "none", md: "flex" }}>
-      {conversationId ? <Flex></Flex> : <div>No Conversation Selected</div>}
+      {conversationId && typeof conversationId === "string" ? (
+        <Flex
+          direction="column"
+          justify="space-between"
+          overflow="hidden"
+          flexGrow={1}
+        >
+          {/* {conversationId} */}
+          <MessagesHeader userId={userId} conversationId={conversationId} />
+        </Flex>
+      ) : (
+        <div>No Conversation Selected</div>
+      )}
     </Flex>
   )
 }
