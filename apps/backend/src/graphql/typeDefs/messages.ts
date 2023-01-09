0b9eea555e4c3ad98669289d76e7b750d9446322
Prabhjot <prabhjottechs@gmail.com>
2023-01-09 17:43:44 +0530

    adding message types to backend

---
+++ apps/backend/src/graphql/typeDefs/messages.ts
@@ -0,0 +1,14 @@
+import gql from "graphql-tag";
+
+const typeDefs = gql`
+scalar Date
+
+type Message {
+  id: String 
+  sender: User
+  body: String
+  createdAt: Date
+  }
+`;
+
+export default typeDefs;
