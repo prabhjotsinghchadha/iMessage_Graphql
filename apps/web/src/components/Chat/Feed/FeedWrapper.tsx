daca63fd6f8411221ccf50ad2b88f6da2c63a8e9
Prabhjot <prabhjottechs@gmail.com>
2023-01-09 17:45:43 +0530

    updating UI of Feed page

--- apps/web/src/components/Chat/Feed/FeedWrapper.tsx
+++ apps/web/src/components/Chat/Feed/FeedWrapper.tsx
@@ -1,12 +1,19 @@
+import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
+import { useRouter } from "next/router";
 
 interface FeedWrapper {
   session: Session;
 }
 
 const FeedWrapper: React.FC<FeedWrapper> = ({ session, }) => {
+  const router = useRouter();
+
+  const { conversationId } = router.query;
   return (
-    <div>ConversationsWrapper</div>
+    <Flex width="100%" direction='column' display={{ base: conversationId ? "flex" : "none", md: "flex" }}>
+      {conversationId ? <Flex></Flex> : <div>No Conversation Selected</div>}
+    </Flex>
   )
 }
 
