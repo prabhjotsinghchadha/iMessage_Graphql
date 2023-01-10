273853678567e9233ebc102db7a177f6b8757bc7
Prabhjot <prabhjottechs@gmail.com>
2023-01-10 14:43:46 +0530

    fixing the style of input

--- apps/web/src/components/Chat/Feed/FeedWrapper.tsx
+++ apps/web/src/components/Chat/Feed/FeedWrapper.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useRouter } from "next/router";
 import MessagesHeader from "./Messages/Header";
+import MessageInput from "./Messages/Input";
 
 interface FeedWrapper {
   session: Session;
@@ -15,15 +16,18 @@ const FeedWrapper: React.FC<FeedWrapper> = ({ session, }) => {
   return (
     <Flex width="100%" direction='column' display={{ base: conversationId ? "flex" : "none", md: "flex" }}>
       {conversationId && typeof conversationId === "string" ? (
-        <Flex
-          direction="column"
-          justify="space-between"
-          overflow="hidden"
-          flexGrow={1}
-        >
-          {/* {conversationId} */}
-          <MessagesHeader userId={userId} conversationId={conversationId} />
-        </Flex>
+        <>
+          <Flex
+            direction="column"
+            justify="space-between"
+            overflow="hidden"
+            flexGrow={1}
+          >
+            {/* {conversationId} */}
+            <MessagesHeader userId={userId} conversationId={conversationId} />
+          </Flex>
+          <MessageInput session={session} conversationId={conversationId} />
+        </>
       ) : (
         <div>No Conversation Selected</div>
       )}
