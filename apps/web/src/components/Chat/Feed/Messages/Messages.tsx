25b24a0f0392412751234efe8c27ef86afda522c
Prabhjot <prabhjottechs@gmail.com>
2023-01-17 20:58:35 +0530

    adding types to ui

--- apps/web/src/components/Chat/Feed/Messages/Messages.tsx
+++ apps/web/src/components/Chat/Feed/Messages/Messages.tsx
@@ -1,7 +1,28 @@
-interface MessagesProps { }
+import { useQuery } from "@apollo/client";
+import { Flex } from "@chakra-ui/react";
+import { MessagesData, MessagesVariables } from "../../../../util/types";
+import MessageOperations from '../../../../graphql/operations/messages';
+import { toast } from "react-hot-toast";
 
-const Messages: React.FC<MessagesProps> = () => {
-  return <div></div>
+interface MessagesProps {
+  userId: string;
+  conversationId: string;
+}
+
+const Messages: React.FC<MessagesProps> = ({ userId, conversationId }) => {
+
+  const { data } = useQuery<MessagesData, MessagesVariables>(MessageOperations.Query.messages, {
+    variables: {
+      conversationId,
+    },
+    onError: ({ message }) => {
+      toast.error(message);
+    },
+  })
+
+  return <Flex direction="column" justify="flex-end" overflow="hidden">
+
+  </Flex>
 }
 
 export default Messages;
