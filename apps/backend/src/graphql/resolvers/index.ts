8a927832db435dcb0a16deae492c142c218e9fd1
Prabhjot <prabhjottechs@gmail.com>
2023-01-18 00:35:09 +0530

    adding scalar date type for gql

--- apps/backend/src/graphql/resolvers/index.ts
+++ apps/backend/src/graphql/resolvers/index.ts
@@ -1,8 +1,9 @@
 import userResolvers from './user';
 import conversationResolvers from './conversation';
 import messageResolvers from './message';
+import scalarResolvers from "./scalars";
 import merge from 'lodash.merge';
 
-const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers);
+const resolvers = merge({}, userResolvers, conversationResolvers, messageResolvers, scalarResolvers);
 
 export default resolvers;
