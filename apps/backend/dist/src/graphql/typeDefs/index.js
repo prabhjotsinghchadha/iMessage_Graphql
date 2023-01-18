511c18a05f7ad8ea66b7927c9314b587587332ff
Prabhjot <prabhjottechs@gmail.com>
2023-01-18 22:41:23 +0530

    Revert "adding dist folder"
    This reverts commit f534409e66fdd992b376d652fa3f1d7d0e0c0dcd.

--- apps/backend/dist/src/graphql/typeDefs/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const users_1 = tslib_1.__importDefault(require("./users"));
-const conversation_1 = tslib_1.__importDefault(require("./conversation"));
-const messages_1 = tslib_1.__importDefault(require("./messages"));
-const typeDefs = [users_1.default, conversation_1.default, messages_1.default];
-exports.default = typeDefs;
-//# sourceMappingURL=index.js.map
