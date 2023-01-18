9282f1eccf39cb6e89b5de01f9526fef0b26c1b1
Prabhjot Singh Chadha <prabhjottechs@gmail.com>
2023-01-18 23:54:58 +0530

    Delete apps/backend/dist/src directory

--- apps/backend/dist/src/graphql/resolvers/scalars.d.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLScalarType } from "graphql";
-declare const resolvers: {
-    Date: GraphQLScalarType<Date | null, any>;
-};
-export default resolvers;
--- apps/backend/dist/src/util/functions.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { CreateUsernameResponse, ParticipantPopulated } from "./types";
-export declare function userIsConversationParticipant(participants: Array<ParticipantPopulated>, userId: string): boolean;
-export declare function verifyAndCreateUsername(args: {
-    userId: string;
-    username: string;
-}, prisma: PrismaClient): Promise<CreateUsernameResponse>;
